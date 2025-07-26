b7894a1e14ba5aee11f46104b781a1fc138e82d2
chltmdgh522 <chltmdgh517@naver.com>
2025-07-26 16:09:09 +0900

    [Feat] 설정 추가

--- backend/mind/src/main/java/mind/MindApplication.java
+++ backend/mind/src/main/java/mind/MindApplication.java
@@ -3,7 +3,7 @@ package mind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableAsync
