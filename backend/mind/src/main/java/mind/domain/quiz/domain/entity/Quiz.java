0c271699ca6ff6e0356c6c02fc76057c33cb6a9c
chltmdgh522 <chltmdgh517@naver.com>
2025-07-26 16:48:03 +0900

    [Feat] 퀴즈, 퀴즈 전체 엔티티 개발

---
+++ backend/mind/src/main/java/mind/domain/quiz/domain/entity/Quiz.java
@@ -0,0 +1,59 @@
+package mind.domain.quiz.domain.entity;
+
+import mind.domain.quiz.domain.entity.convert.DurationStringConverter;
+import mind.domain.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@DynamicUpdate
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_record_id")
+    private QuizRecord quizRecord;
+
+    @Column(nullable = false)
+    private boolean isComplete;
+
+    @Column(nullable = false)
+    private Long quizNumber;
+
+    @Column(nullable = false)
+    @Convert(converter = DurationStringConverter.class)
+    private Duration quizTime = Duration.ZERO;
+
+    // 오답 횟수
+    @Column(nullable = false)
+    private Long wrongCnt;
+
+    @Builder
+    public Quiz(User user,
+                QuizRecord quizRecord,
+                boolean isComplete,
+                Long quizNumber,
+                Long wrongCnt) {
+        this.user = user;
+        this.quizRecord = quizRecord;
+        this.isComplete = isComplete;
+        this.quizNumber = quizNumber;
+        this.wrongCnt = wrongCnt;
+    }
+}
