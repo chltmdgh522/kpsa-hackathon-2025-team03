0fed0d2cbca74e87987591bf14baf7a264a475fb
chltmdgh522 <chltmdgh517@naver.com>
2025-07-26 16:42:51 +0900

    [Fix] 에러 커스텀 수정

---
+++ backend/mind/src/main/java/mind/global/infra/exception/error/MindException.java
@@ -0,0 +1,31 @@
+package mind.global.infra.exception.error;
+
+
+public class MindException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public MindException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public MindException(ErrorCode errorCode, String detailMessage) {
+        super(errorCode.getMessage() + " → " + detailMessage);
+        this.errorCode = errorCode;
+    }
+
+
+    public MindException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getMessage(), cause);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public int getHttpStatusCode() {
+        return errorCode.getHttpCode();
+    }
+}
