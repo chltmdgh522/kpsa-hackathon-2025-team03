a910ab90cffc2411935fcb860671995eabb45175
chltmdgh522 <chltmdgh517@naver.com>
2025-07-26 14:23:21 +0900

    [Init] 초기 세팅

---
+++ backend/mind/src/test/java/mind/mind/MindApplicationTests.java
@@ -0,0 +1,13 @@
+package mind.mind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
